0d9da840afc42ad66b33af2f2e32b4b6357d4bca
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2026-02-10 11:55:41 +0530

    Print Rectangle Pattern

---
+++ LogicalThinkingPatterns/RectanglePattern.java
@@ -0,0 +1,21 @@
+package LogicalThinkingPatterns;
+
+import java.util.Scanner;
+
+public class RectanglePattern {
+    private static void printPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        printPattern(n);
+        in.close();
+    }
+}
