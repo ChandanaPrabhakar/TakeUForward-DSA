bc768da79c4091051c139a553ab147281e4cab77
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2026-02-10 12:07:23 +0530

    Right Angles Triangle Pattern

---
+++ LogicalThinkingPatterns/TrianglePattern/TrianglePattern.java
@@ -0,0 +1,21 @@
+package LogicalThinkingPatterns.TrianglePattern;
+
+import java.util.Scanner;
+
+public class TrianglePattern {
+    private static void printPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        printPattern(n);
+        in.close();
+    }
+}
